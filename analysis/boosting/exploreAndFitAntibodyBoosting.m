8c5511dba58c6c5bd4da64d14c55a0118bdcd4ac
famulare <mikefamulare@gmail.com>
2018-03-26 17:42:50 -0700

    Updated code to use python-like datasets and reconcile log_e vs log_2 theta

--- analysis/boosting/exploreAndFitAntibodyBoosting.m
+++ analysis/boosting/exploreAndFitAntibodyBoosting.m
@@ -8,41 +8,53 @@ addpath(genpath('..\..\data\Louisiana1957'));
 
 [data,hdrStr]=xlsread('householdSeroconversion.xlsx','prePostAntibodyLineList');
 hdrStr=hdrStr(1,1:2);
-data=log2(data);
-hdrStr={'preExposureLog2NAb','postExposureLog2NAb'};
+
 
 %log diff
-hdrStr{3}='log2Diff';
-data(:,3)=data(:,2)-data(:,1);
+hdrStr{3}='postPreRatio';
+data(:,3)=data(:,2)./data(:,1);
 
 df=mat2dataset(data,'varnames',hdrStr);
 
+df.log2PreExposureTiter = log2(df.preExposureTiter);
+df.log2PostExposureTiter = log2(df.postExposureTiter);
+df.logePostPreRatio = log(df.postPreRatio);
+
 %% explore non-infected data
 seroconversionThreshold=4; % 
-idx=df.log2Diff>=log2(seroconversionThreshold);
+idx=df.postPreRatio>=seroconversionThreshold;
 
-LM=fitlm(df(idx,:),'log2Diff~preExposureLog2NAb')
+LM=fitlm(df(idx,:),'logePostPreRatio ~ log2PreExposureTiter')
 
 figure(1); clf; hold all;
 
-jitter=0.1*randn(length(df.log2Diff(idx)),2);
+jitter=0.1*randn(length(df.postPreRatio(idx)),2);
 
 subplot(1,2,1); hold all;
 h=LM.plot();
 set(h(1),'visible','off')
-plot(df.preExposureLog2NAb(idx)+jitter(:,1),df.log2Diff(idx)+jitter(:,2),'x')
+plot(df.log2PreExposureTiter(idx)+jitter(:,1),df.logePostPreRatio(idx)+jitter(:,2),'x')
 
 subplot(1,2,2); hold all;
-test=mat2dataset([0:14]','varnames','preExposureLog2NAb');
+test=mat2dataset([0:15]','varnames','log2PreExposureTiter');
 [test.theta,test.CI]=LM.predict(test);
-test.theta=2.^test.theta;
-test.CI=2.^test.CI;
-plotWithCI(test.preExposureLog2NAb,test.theta,test.CI,'r')
+test.theta=exp(test.theta);
+test.CI=exp(test.CI);
+plotWithCI(test.log2PreExposureTiter,test.theta,test.CI,'r')
 ylabel('theta')
-xlabel('preInfection log2NAb')
+xlabel('log2PreExposureTiter')
 title({'WPV mean response model','from Gelfand 1957b'})
 
+% jafari
+plot(test.log2PreExposureTiter,exp(4.28-.2*test.log2PreExposureTiter),'k')
 
+% inset
+axes('position',[.7,.5,.25,.3]); hold on
+plotWithCI(test.log2PreExposureTiter,test.theta,test.CI,'r')
+plot(test.log2PreExposureTiter,exp(4.28-.2*test.log2PreExposureTiter),'k')
+ylabel('theta')
+xlabel('log2PreExposureTiter')
+xlim([6,14])
 
 %% is log titer for non-coverters roughly gaussian?
 figure(2); hold all;
